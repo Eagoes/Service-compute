bcb02ef37d70c899536a70de07f541cd0c12b98c
Eric K. Jobs <445039992@qq.com>
2019-10-18 15:04:07 +0800

    fix usr-result.html, change version number to 1.0

--- TravelAgencySystem/src/main/java/com/service/client/controller/PlannerController.java
+++ TravelAgencySystem/src/main/java/com/service/client/controller/PlannerController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -149,16 +150,158 @@ public class PlannerController {
 		modelAndView.addObject("resultList", resultList);
         return modelAndView;
     }
+
+
     @RequestMapping(value = "/usr-result.html", method = RequestMethod.GET)
-    public ModelAndView showUsrResult(@Valid Userrequest userrequest, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView();
-        System.out.println(userrequest.getAim());
-        System.out.println(Integer.toString(userrequest.getCost()));
-        System.out.println(userrequest.getDeparture_time());
-        System.out.println(userrequest.getDepature_point());
-        System.out.println(userrequest.getReturn_time());
-        modelAndView.setViewName("usr-query");
-        return modelAndView;
+    public ModelAndView showUsrResult(@RequestParam("departureAirlineId") long departureAirlineId,
+									  @RequestParam("returnAirlineId") long returnAirlineId,
+									  @RequestParam("hotelId") long hotelId,
+									  @RequestParam("carRentalId") long carRentalId,
+									  @RequestParam("attractionId") long attractionId,
+									  @RequestParam("guideId") long guideId) {
+		ModelAndView modelAndView = new ModelAndView();
+		List<AgentService> agentServices = agentServiceService.findAll();
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new StringHttpMessageConverter(Charset.forName("UTF-8")));
+
+		Airline finalRetAirline = null;
+		Airline finalDepAirline = null;
+		Attraction finalAttraction = null;
+		Guide finalGuide = null;
+		Hotel finalHotel = null;
+		CarRental finalCarRental = null;
+		float cost = 0;
+
+		for (AgentService as : agentServices) {
+			if (as.getServiceType().equals("Airline")) {
+				String planOption = "getAirline";
+				String serviceURL = "http://" + as.getServiceIP() + ":8080/" + as.getServiceName() + "/" + planOption + "?id={departureAirlineId}";
+
+				Map<String, Long> departureMap = new HashMap<String, Long>();
+				departureMap.put("departureAirlineId", new Long(departureAirlineId));
+				finalDepAirline = restTemplate.getForObject(serviceURL, Airline.class, departureMap);
+
+				serviceURL = "http://" + as.getServiceIP() + ":8080/" + as.getServiceName() + "/" + planOption + "?id={returnAirlineId}";
+				Map<String, Long> returnMap = new HashMap<String, Long>();
+				returnMap.put("returnAirlineId", new Long(returnAirlineId));
+				finalRetAirline = restTemplate.getForObject(serviceURL, Airline.class, returnMap);
+			}
+
+			if (as.getServiceType().equals("Hotel")) {
+				String planOption = "findHotelById";
+				String serviceURL = "http://" + as.getServiceIP() + ":8080/" + as.getServiceName() + "/" + planOption + "?id={hotelId}";
+//				System.out.println(serviceURL);
+				Map<String, Long> map = new HashMap<String, Long>();
+				map.put("hotelId", new Long(hotelId));
+				finalHotel = restTemplate.getForObject(serviceURL, Hotel.class, map);
+			}
+
+			if (as.getServiceType().equals("Attraction")) {
+				String planOption = "getAttraction";
+				String serviceURL = "http://" + as.getServiceIP() + ":8080/" + as.getServiceName() + "/" + planOption + "?id={attractionId}";
+				Map<String, Long> map = new HashMap<String, Long>();
+				map.put("attractionId", new Long(attractionId));
+				finalAttraction = restTemplate.getForObject(serviceURL, Attraction.class, map);
+			}
+
+			if (as.getServiceType().equals("CarRental")) {
+				String planOption = "findCarById";
+				String serviceURL = "http://" + as.getServiceIP() + ":8080/" + as.getServiceName() + "/" + planOption + "?id={carRentalId}";
+				Map<String, Long> map = new HashMap<String, Long>();
+				map.put("carRentalId", new Long(carRentalId));
+				finalCarRental = restTemplate.getForObject(serviceURL, CarRental.class, map);
+			}
+
+			if (as.getServiceType().equals("Guide")) {
+				String planOption = "findGuideById";
+				String serviceURL = "http://" + as.getServiceIP() + ":8080/" + as.getServiceName() + "/" + planOption + "?id={guideId}";
+				Map<String, Long> map = new HashMap<String, Long>();
+				map.put("guideId", new Long(guideId));
+				finalGuide = restTemplate.getForObject(serviceURL, Guide.class, map);
+			}
+		}
+
+		if(finalDepAirline!=null) {
+			modelAndView.addObject("departure_airline_message",
+					"Departure City：" + finalDepAirline.getDeparture() + "<br/>" +
+							"Departure Date：" + finalDepAirline.getDepartureTime() + "<br/>" +
+							"Destination：" + finalDepAirline.getDestination() + "<br/>" +
+							"Airline Price：" + finalDepAirline.getAirlinePrice());
+			cost = cost + finalDepAirline.getAirlinePrice();
+		}
+		else
+		{
+			System.out.println("departure airline id:" + departureAirlineId + " is null");
+			modelAndView.addObject("departure_airline_message","");
+		}
+
+		if(finalRetAirline!=null) {
+			System.out.println("print return airline");
+			modelAndView.addObject("return_airline_message",
+					"Return City：" + finalRetAirline.getDeparture() + "<br/>" +
+							"Return Date：" + finalRetAirline.getDepartureTime() + "<br/>" +
+							"Destination：" + finalRetAirline.getDestination() + "<br/>" +
+							"Price：" + finalRetAirline.getAirlinePrice());
+			cost = cost + finalRetAirline.getAirlinePrice();
+		}
+		else {
+			System.out.println("return  airline id:" + returnAirlineId + " is null");
+			modelAndView.addObject("return_airline_message","");
+		}
+
+		if(finalHotel!=null) {
+			modelAndView.addObject("hotel_message",
+					"Hotel Name：" + finalHotel.getName() + "<br/>" +
+							"Hotel Address：" + finalHotel.getAddress() + "<br/>" +
+							"Hotel Level：" + finalHotel.getHotelStar() + "<br/>" +
+							"Hotel Price：" + finalHotel.getHotelPrice());
+			cost = cost + finalHotel.getHotelPrice();
+		}
+		else {
+			modelAndView.addObject("hotel_message", "");
+		}
+
+		if(finalCarRental!=null) {
+			modelAndView.addObject("car_message",
+					"Car Brand：" + finalCarRental.getBrand() + "<br/>" +
+							"Car Type：" + finalCarRental.getCarType() + "<br/>" +
+							"Car Plate：" + finalCarRental.getCarPlate() + "<br/>" +
+							"Rental Location：" + finalCarRental.getRentalLoc() + "<br/>" +
+							"Return Location：" + finalCarRental.getReturnLoc() + "<br/>" +
+							"Rental Price：" + finalCarRental.getCarPrice());
+			cost = cost + finalCarRental.getCarPrice();
+		}
+		else {
+			modelAndView.addObject("car_message", "");
+		}
+
+		if(finalAttraction!=null) {
+			modelAndView.addObject("attraction_message",
+					"Attraction Name：" + finalAttraction.getName() + "<br/>" +
+							"Attraction Phone：" + finalAttraction.getPhone() + "<br/>" +
+							"Attraction Address：" + finalAttraction.getAttractionAddress() + "<br/>" +
+							"Attraction Level：" + finalAttraction.getAttractionStar() + "<br/>" +
+							"Attraction Price：" + finalAttraction.getAttractionPrice());
+			cost = cost + finalAttraction.getAttractionPrice();
+		}
+		else {
+			modelAndView.addObject("attraction_message", "");
+		}
+
+		if(finalGuide!=null) {
+			modelAndView.addObject("guide_message",
+					"Guide Name：" + finalGuide.getName() + "<br/>" +
+							"Guide Phone：" + finalGuide.getPhone() + "<br/>" +
+							"Guide Price：" + finalGuide.getPrice());
+			cost = cost + finalGuide.getPrice();
+		}
+		else {
+			modelAndView.addObject("guide_message", "");
+		}
+
+
+		modelAndView.addObject("cost","Cost：" + cost);
+		return modelAndView;
     }
     
     @RequestMapping(value = "/test", method = RequestMethod.GET)
--- TravelAgencySystem/src/main/resources/data.sql
+++ TravelAgencySystem/src/main/resources/data.sql
@@ -4,8 +4,8 @@ REPLACE INTO `role` VALUES (2,'USER');
 replace into `t_user` values (100,"hello","123456","1339170","xinxi@ruc.edu.cn",1);
 
 
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Airline", "Airline-0.0.1-SNAPSHOT", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Attraction", "Attraction-0.0.1-SNAPSHOT", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "CarRental", "CarRental-0.0.1-SNAPSHOT", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Guide", "Guide-0.0.1-SNAPSHOT", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Hotel", "Hotel-0.0.1-SNAPSHOT", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Airline", "Airline-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Attraction", "Attraction-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "CarRental", "CarRental-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Guide", "Guide-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Hotel", "Hotel-1.0", "10.77.50.78");
