7fd964b3996136435050bcce9a0fb292b5f680e9
Eric K. Jobs <445039992@qq.com>
2019-10-18 15:45:18 +0800

    fix some bug of service table

--- TravelAgencySystem/src/main/resources/data.sql
+++ TravelAgencySystem/src/main/resources/data.sql
@@ -4,8 +4,8 @@ REPLACE INTO `role` VALUES (2,'USER');
 replace into `t_user` values (100,"hello","123456","1339170","xinxi@ruc.edu.cn",1);
 
 
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Airline", "Airline-1.0", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Attraction", "Attraction-1.0", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "CarRental", "CarRental-1.0", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Guide", "Guide-1.0", "10.77.50.78");
-replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(0, "Hotel", "Hotel-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(1, "Airline", "Airline-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(2, "Attraction", "Attraction-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(3, "CarRental", "CarRental-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(4, "Guide", "Guide-1.0", "10.77.50.78");
+replace into `agentServices`(serviceID, serviceType, serviceName, serviceIP) value(5, "Hotel", "Hotel-1.0", "10.77.50.78");
