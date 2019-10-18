5d4d279c6652197f3ff330aaa144c2c4488da561
Eric K. Jobs <445039992@qq.com>
2019-10-18 08:02:09 +0800

    change Airline's data.sql

--- Airline/src/main/resources/data.sql
+++ Airline/src/main/resources/data.sql
@@ -2,24 +2,24 @@ replace into `airlines`(airlineid,airlineName,departure,destination,departureTim
 	values(1,"AL001","NanChang","BeiJing","2019-12-12","2019-12-12",800,9,7,1);
 replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(3,"AL001","NanChang","BeiJing","2019-12-14","2019-12-14",800,9,7,1);
-replace into `airlines`(airlineid,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
+replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(2,"AL002","BeiJing","NanChang","2019-12-14","2019-12-14",800,9,7,1);
-replace into `airlines`(airlineid,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
+replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(4,"AL002","BeiJing","NanChang","2019-12-14","2019-12-14",800,9,7,1);
 
 replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(5,"DB001","ShangHai","BeiJing","2019-12-12","2019-12-12",1000,9,7,1);
 replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(6,"DB001","ShangHai","BeiJing","2019-12-14","2019-12-14",1000,9,7,1);
-replace into `airlines`(airlineid,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
+replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(7,"DB002","BeiJing","ShangHai","2019-12-14","2019-12-14",1000,9,7,1);
-replace into `airlines`(airlineid,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
+replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(8,"DB002","BeiJing","ShangHai","2019-12-14","2019-12-14",1000,9,7,1);
 replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(9,"DB001","ShangHai","BeiJing","2019-12-12","2019-12-12",1000,9,7,1);
 replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(10,"DB001","ShangHai","BeiJing","2019-12-14","2019-12-14",1000,9,7,1);
-replace into `airlines`(airlineid,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
+replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(11,"DB002","BeiJing","ShangHai","2019-12-14","2019-12-14",1000,9,7,1);
-replace into `airlines`(airlineid,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
+replace into `airlines`(airlineid,airlineName,departure,destination,departureTime,arrivalTime,airlinePrice,remainNum,duration,status)
 	values(12,"DB002","BeiJing","ShangHai","2019-12-14","2019-12-14",1000,9,7,1);
